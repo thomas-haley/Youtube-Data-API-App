4113dd85841e7c68b81c8d91eaf5476f96328d0f
thomas-haley <thomas.haley.2018@gmail.com>
2024-11-11 16:40:29 -0500

    JWT Token Implementation

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,21 +1,23 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController(DataContext context) : ControllerBase
+[Authorize]
+public class UsersController(DataContext context) : BaseAPIController
 {
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
         var users = await context.Users.ToListAsync();
         return users;
     }
 
+    [Authorize]
     [HttpGet("{id:int}")]
     public async Task<ActionResult<AppUser>> GetUser(int id){
         var user = await context.Users.FindAsync(id);
